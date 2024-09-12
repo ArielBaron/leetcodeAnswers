4dc6408d4e4ca1999a46e3b4b7098e8be55f3fda
ArielBaron <136897420+ArielBaron@users.noreply.github.com>
2024-09-12 21:03:04 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,7 +4,7 @@ def Main():
         seen_lst = []
         last_index = 0
         for index in range(len(s)):
-            for index, char in enumerate(s[last_index:len(s)]):
+            for char in s[last_index:len(s)]:
                 if char not in seen_lst:
                     seen_lst.append(char)
                 else:
@@ -13,6 +13,7 @@ def Main():
             if len(seen_lst) > longest_sub_string:
                 longest_sub_string = len(seen_lst)
             if longest_sub_string > len(s[last_index:len(s)]):
+                print(longest_sub_string)
                 return longest_sub_string
             seen_lst = []
 
