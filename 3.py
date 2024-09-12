b263475ac023c0b68d25cf74cfd1d05f13655f5b
ArielBaron <136897420+ArielBaron@users.noreply.github.com>
2024-09-12 20:55:23 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,23 @@
+def Main():
+    def Longest_Substring_Without_Repeating_Characters(s):
+        longest_sub_string = 1
+        seen_lst = []
+        last_index = 0
+        for index in range(len(s)):
+            for index, char in enumerate(s[last_index:len(s)]):
+                if char not in seen_lst:
+                    seen_lst.append(char)
+                else:
+                    break
+            last_index += len(seen_lst)
+            if len(seen_lst) > longest_sub_string:
+                longest_sub_string = len(seen_lst)
+            if longest_sub_string > len(s[last_index:len(s)]):
+                return longest_sub_string
+            seen_lst = []
+
+        print(longest_sub_string)
+        return longest_sub_string
+    return Longest_Substring_Without_Repeating_Characters("pwwkew")
+
+Main()
